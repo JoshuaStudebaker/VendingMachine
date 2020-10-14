34c904da844aa89ee737fdce9b6d4a8fc42db50b
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-14 14:12:54 -0600

    chips model proto

--- Models/FoodItem.cs
+++ Models/FoodItem.cs
@@ -6,11 +6,15 @@ namespace VendingMachine.Models
   abstract class FoodItem //: IBuyable
   {
     public string Name { get; set; }
-    public bool isEdible { get; set; }
-    public FoodItem(string name)
+    public string Description { get; set; }
+    public bool IsEdible { get; set; }
+    public double Price { get; set; }
+    public FoodItem(string name, string description, double price)
     {
       Name = name;
-      isEdible = true;
+      IsEdible = true;
+      Description = description;
+      Price = price;
     }
   }
 }
